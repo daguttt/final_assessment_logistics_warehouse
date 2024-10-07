6c87cc39160bb85f3dca3a22cc43342fb1ecb756
SoyNar <narciris.tech@gmail.com>
2024-10-07 17:47:16 -0500

    feat: add setter and constructos to `ApiResponseDto`

--- src/main/java/com/riwi/final_assessment_logistics_warehouse/common/infrastructure/dtos/response/ApiResponseDto.java
+++ src/main/java/com/riwi/final_assessment_logistics_warehouse/common/infrastructure/dtos/response/ApiResponseDto.java
@@ -1,9 +1,15 @@
 package com.riwi.final_assessment_logistics_warehouse.common.infrastructure.dtos.response;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.experimental.SuperBuilder;
 
 @Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @SuperBuilder
 public abstract class ApiResponseDto<TData> {
     private Integer status;
