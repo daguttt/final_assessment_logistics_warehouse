9422809f00f0ef17598c1a9df8b340c617599ce2
SoyNar <narciris.tech@gmail.com>
2024-10-07 15:42:41 -0500

    feat: enable auditing and add `AuditableEntity` as base entity

---
+++ src/main/java/com/riwi/final_assessment_logistics_warehouse/common/domain/AuditableEntity.java
@@ -0,0 +1,49 @@
+package com.riwi.final_assessment_logistics_warehouse.common.domain;
+
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import com.riwi.final_assessment_logistics_warehouse.users.domain.UserEntity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.OneToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Setter
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+public abstract class AuditableEntity {
+  @CreatedDate
+  @Column(nullable = false, updatable = false, columnDefinition = "DATETIME(6) DEFAULT CURRENT_TIMESTAMP(6)")
+  @Builder.Default
+  private LocalDateTime createdAt = LocalDateTime.now();
+
+  @LastModifiedDate
+  @Column(columnDefinition = "DATETIME(6) DEFAULT CURRENT_TIMESTAMP(6)")
+  @Builder.Default
+  private LocalDateTime modifiedAt = LocalDateTime.now();
+
+  @CreatedBy
+  @OneToOne
+  private UserEntity createdBy;
+
+  @LastModifiedBy
+  @OneToOne
+  private UserEntity modifiedBy;
+}
---
+++ src/main/java/com/riwi/final_assessment_logistics_warehouse/common/infrastructure/persistence/JpaConfig.java
@@ -0,0 +1,9 @@
+package com.riwi.final_assessment_logistics_warehouse.common.infrastructure.persistence;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+}
