90c8654159f39f20276e843121e75f6e515f24c9
SoyNar <narciris.tech@gmail.com>
2024-10-07 15:56:58 -0500

    feat: add utility to send emails

---
+++ src/main/java/com/riwi/final_assessment_logistics_warehouse/common/infrastructure/AppConfig.java
@@ -0,0 +1,10 @@
+package com.riwi.final_assessment_logistics_warehouse.common.infrastructure;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@Configuration
+@EnableAsync
+public class AppConfig {
+
+}
