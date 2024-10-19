3bbabd1780704992d0b307ab6901ed5d7ea0429e
daguttt <dagutmu667@gmail.com>
2024-10-19 17:15:50 -0500

    feat: add exception handler to catch when users don't have the permission to access a resource

--- src/main/java/com/riwi/final_assessment_logistics_warehouse/common/infrastructure/exception_handlers/SecurityExceptionHandler.java
+++ src/main/java/com/riwi/final_assessment_logistics_warehouse/common/infrastructure/exception_handlers/SecurityExceptionHandler.java
@@ -2,6 +2,7 @@ package com.riwi.final_assessment_logistics_warehouse.common.infrastructure.exce
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -16,6 +17,12 @@ public class SecurityExceptionHandler {
     @ExceptionHandler(BadCredentialsException.class)
     public ProblemDetail handleBadCredentialException(BadCredentialsException exception) {
         return ProblemDetail.forStatusAndDetail(HttpStatus.UNAUTHORIZED, "Invalid email or password");
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ProblemDetail handleAccessDeniedException(AccessDeniedException exception) {
+        return ProblemDetail.forStatusAndDetail(HttpStatus.FORBIDDEN,
+                "You don't have permission to access this resource");
     }
 
     @ExceptionHandler(ExpiredJwtException.class)
